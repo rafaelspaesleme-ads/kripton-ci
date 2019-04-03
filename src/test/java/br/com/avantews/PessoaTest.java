b3511d97a416c068c41b26cd4a0622a3400eb8fd
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-04-02 22:53:08 -0300

    Editando entidade Pessoa e teste da mesma

--- src/test/java/br/com/avantews/PessoaTest.java
+++ src/test/java/br/com/avantews/PessoaTest.java
@@ -1,10 +1,13 @@
 package test.java.br.com.avantews;
 
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class PessoaTest {
 
     @Test
     public void criaEntityPessoa(){
-        
+
     }
     
 }
