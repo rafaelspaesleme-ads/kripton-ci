ec0216df4151d525a0fca2f1db3ebb71a0df3310
Rafael Paes Leme <rafaelspaesleme.ads@gmail.com>
2019-04-02 23:15:53 -0300

    Correct teste Pessoa

--- src/test/java/br/com/avantews/PessoaTest.java
+++ src/test/java/br/com/avantews/PessoaTest.java
@@ -1,13 +1,46 @@
-package test.java.br.com.avantews;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.avantews;
 
-import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
+/**
+ *
+ * @author avantews
+ */
 public class PessoaTest {
+    
+    public PessoaTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
 
     @Test
-    public void criaEntityPessoa(){
-
+    public void testSomeMethod() {
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
     }
     
 }
